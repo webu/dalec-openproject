ec318d25d8a8656f143eb90941b59fda672db075
Samuel <s.weber@webu.coop>
2021-11-05 17:34:55 +0100

    fetch work package of a project

--- dalec_openproject/proxy.py
+++ dalec_openproject/proxy.py
@@ -9,6 +9,7 @@ from django.conf import settings
 from dalec.proxy import Proxy
 
 from pyopenproject.openproject import OpenProject
+from pyopenproject.model.project import Project
 
 client = OpenProject(
         url=settings.DALEC_OPENPROJECT_BASE_URL,
@@ -26,17 +27,48 @@ class OpenprojectProxy(Proxy):
     def _fetch(
         self, nb: int, content_type: str, channel: str, channel_object: str
     ) -> Dict[str, dict]:
-        if content_type == "event":
-            return self._fetch_event(nb, channel, channel_object)
+        if content_type == "work_package":
+            return self._fetch_work_packages(nb, channel, channel_object)
 
-        raise ValueError(f"Invalid content_type {content_type}. Accepted: event." )
+        raise ValueError(f"Invalid content_type {content_type}. Accepted: work_package." )
 
-    def _fetch_event(self, nb, channel=None, channel_object=None):
+    def _fetch_work_packages(self, nb, channel=None, channel_object=None):
         """
-        Get latest event from calendar
+        Get latest work packages
         """
         options = {
                 "per_page": nb,
                 }
+        
+        if channel != "project":
+            raise ValueError(
+                    f"""
+                    channel is invalid for work_package. It must be : project .
+                    """
+                    )
 
-        # TODO
+        project_identifier = channel_object
+        
+        project = Project({"identifier": project_identifier})
+
+        project_service = client.get_project_service()
+        work_packages = project_service.find_work_packages(project)
+
+        contents = {}
+        for wp in work_packages:
+            content = {
+                    attr: getattr(wp, attr)
+                    for attr in dir(wp)
+                    if not attr.startswith("__")
+                    }
+            content.update(
+                    {
+                        "id": str(wp.id),
+                        "creation_dt": wp.createdAt,
+                        "last_update_dt": now()
+                        }
+                    )
+            contents[str(wp.id)] = content
+        return contents
+
+        
